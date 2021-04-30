ae85abf5646e4ed9968989552847eaf6f21ad5d9
ve5li <ve5li@tuta.io>
2021-04-30 10:25:34 +0200

    changed websocket to local ip again

--- hatsune/scripts/main.js
+++ hatsune/scripts/main.js
@@ -6,7 +6,7 @@ var stage = new Konva.Stage({
     height: canvasHeight
 });
 
-let socket = new WebSocket("ws://192.168.178.80:8765");
+let socket = new WebSocket("ws://pi-server:8765");
 //var bannerLayer = addBannerLayer(stage, socket);
 var controlLayer = addControlLayer(stage, socket, offsetLayers);
 var messageLayer = addMessageLayer(stage, socket, offsetLayers);
